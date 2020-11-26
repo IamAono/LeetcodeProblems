376ba3a474d718af53e1c8ba7193632fc76e9776
Stephen Long <stephenlong7@gmail.com>
2020-11-26 14:25:02 -0800

    no message

--- Easy difficulty/RomanToInteger.cpp
+++ Easy difficulty/RomanToInteger.cpp
@@ -3,51 +3,26 @@ public:
 	vector<char> roman;
 	vector<int> values;
 	int romanToInt(string s) {
-		roman.push_back('I');
-		roman.push_back('V');
-		roman.push_back('X');
-		roman.push_back('L');
-		roman.push_back('C');
-		roman.push_back('D');
-		roman.push_back('M');
-		values.push_back(1);
-		values.push_back(5);
-		values.push_back(10);
-		values.push_back(50);
-		values.push_back(100);
-		values.push_back(500);
-		values.push_back(1000);
-		char prev;
-		int prevI;
+		unordered_map<char, int> um;
+		um['I'] = 1;
+		um['V'] = 5;
+		um['X'] = 10;
+		um['L'] = 50;
+		um['C'] = 100;
+		um['D'] = 500;
+		um['M'] = 1000;
+		int prev = 0;
 		int value = 0;
 		for (int i = s.length() - 1; i >= 0; i--) {
 			char c = s[i];
-			int cVal;
-			int cI;
-			for (int j = 0; j < roman.size(); j++) {
-				if (i != s.length() - 1) {
-					if (roman.at(j) == prev) {
-						prevI = j;
-					}
-				}
-				if (roman.at(j) == c) {
-					cI = j;
-					cVal = values.at(j);
-				}
-			}
-			if (i != s.length() - 1) {
-				if (cI < prevI) {
-					value -= values.at(cI);
-				}
-				else {
-					value += cVal;
-				}
+			int val = um[c];
+			if (prev > val) {
+				value -= val;
 			}
 			else {
-				value += cVal;
+				value += val;
 			}
-			prev = c;
-			prevI = cI;
+			prev = val;
 		}
 		return value;
 	}
