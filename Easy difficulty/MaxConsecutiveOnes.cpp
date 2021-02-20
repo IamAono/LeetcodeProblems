95a9970c851c094b4780815f08cd3b71072fd674
Stephen Long <stephenlong7@gmail.com>
2021-02-19 20:26:11 -0800

    24 ms runtime

--- Easy difficulty/MaxConsecutiveOnes.cpp
+++ Easy difficulty/MaxConsecutiveOnes.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-	int findMaxConsecutiveOnes(vector<int>& nums) {
-		int count = 0;
-		int max = 0;
-		for (int i = 0; i < nums.size(); i++) {
-			if (nums.at(i) == 1 && count + 1 > max && i == nums.size() - 1) {
-				max = count + 1;
-			}
-			else if (nums.at(i) == 1) {
-				count++;
-			}
-			else if (count > max) {
-				max = count;
-				count = 0;
-			}
-			else {
-				count = 0;
-			}
-		}
-		return max;
-	}
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                ans = max(ans, count);
+                count = 0;
+            }
+            else if (nums[i] == 1) {
+                count++;
+            }
+        }
+        ans = max(ans, count);
+        return ans;
+    }
 };
